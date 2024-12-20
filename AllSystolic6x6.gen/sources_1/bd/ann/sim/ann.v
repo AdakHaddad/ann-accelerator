7b3f7f7706675425464512046ebe2c40b5c246d1
AdakHaddad <muhammadmuqtada@gmail.com>
2024-12-20 13:44:01 +0700

    first commit

---
+++ AllSystolic6x6.gen/sources_1/bd/ann/sim/ann.v
@@ -0,0 +1,18 @@
+//Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+//Copyright 2022-2024 Advanced Micro Devices, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2024.1 (win64) Build 5076996 Wed May 22 18:37:14 MDT 2024
+//Date        : Mon Dec 16 16:16:03 2024
+//Host        : DESKTOP-HTVV1N1 running 64-bit major release  (build 9200)
+//Command     : generate_target ann.bd
+//Design      : ann
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+(* CORE_GENERATION_INFO = "ann,IP_Integrator,{x_ipVendor=xilinx.com,x_ipLibrary=BlockDiagram,x_ipName=ann,x_ipVersion=1.00.a,x_ipLanguage=VERILOG,numBlks=0,numReposBlks=0,numNonXlnxBlks=0,numHierBlks=0,maxHierDepth=0,numSysgenBlks=0,numHlsBlks=0,numHdlrefBlks=0,numPkgbdBlks=0,bdsource=USER,synth_mode=Hierarchical}" *) (* HW_HANDOFF = "ann.hwdef" *) 
+module ann
+   ();
+
+
+endmodule
---
+++ AllSystolic6x6.gen/sources_1/bd/design_2/hdl/design_2_wrapper.v
@@ -0,0 +1,19 @@
+//Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+//Copyright 2022-2024 Advanced Micro Devices, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2024.1 (win64) Build 5076996 Wed May 22 18:37:14 MDT 2024
+//Date        : Wed Dec 18 16:45:31 2024
+//Host        : DESKTOP-HTVV1N1 running 64-bit major release  (build 9200)
+//Command     : generate_target design_2_wrapper.bd
+//Design      : design_2_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module design_2_wrapper
+   ();
+
+
+  design_2 design_2_i
+       ();
+endmodule
